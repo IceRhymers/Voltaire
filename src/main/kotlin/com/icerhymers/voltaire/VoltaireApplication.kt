eef0c0f71337554c001026091a2e3d5a8f174035
Tanner Wendland <tanner.wendland@gmail.com>
2023-05-31 12:03:57 -0500

    Turned off some auto configurations to allow the app to build and run with the spring context.

--- src/main/kotlin/com/icerhymers/voltaire/VoltaireApplication.kt
+++ src/main/kotlin/com/icerhymers/voltaire/VoltaireApplication.kt
@@ -1,9 +1,10 @@
 package com.icerhymers.voltaire
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class VoltaireApplication
 
 fun main(args: Array<String>) {
